ca726b355daaadd88397ffbd128329f504a3e022
kqlqk <kononchuk99999@gmail.com>
2022-10-18 22:02:31 +0200

    prepare before deploy on tomcat

--- src/main/java/me/kqlqk/behealthy/eureka/EurekaServer.java
+++ src/main/java/me/kqlqk/behealthy/eureka/EurekaServer.java
@@ -2,7 +2,6 @@ package me.kqlqk.behealthy.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
@@ -12,10 +11,5 @@ public class EurekaServer extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(EurekaServer.class);
     }
 }
